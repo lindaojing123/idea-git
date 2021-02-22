f714521fb6ecf04feb4454dda7733b841036576d
lindaojing <857070484@qq.com>
2021-02-22 19:13:35 +0800

    新建项目springboot环境

---
+++ src/test/java/com/ideagit/IdeaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideagit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
